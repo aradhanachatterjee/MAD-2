c69b420bfa9ca063a24afad28273d790296ea217
ara <22f3000701@ds.study.iitm.ac.in>
2024-06-20 06:44:08 +0530

    2.4 ✅

--- week 2/objects.js
+++ week 2/objects.js
@@ -20,19 +20,119 @@
 // console.log(xx.f(10));
 
 /***
- * Copyinh
+ * Copying
+*/
+
+// let x = {'a': 1, 'b': 2};
+// let y = x;
+// console.log(x);
+// console.log(y);
+// x.a = 3;
+// console.log(x);
+// console.log(y);
+
+// let z = {...x};
+// x.a = 5;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+/***
+ * get and set properties
+*/
+
+// let user = {
+//     first: 'Albert',
+//     last: 'Pinto',
+//     get full() {
+//         return this.first + ' ' + this.last;
+//     },
+//     set full(f) {
+//         const parts = f.split(' ');
+//         this.first = parts[0];
+//         this.last = parts[1];
+//     }
+// }
+// console.log(user.full);
+// user.full = 'John Doe';
+// console.log(`Now ${user.first} and ${user.last}`);
+
+
+/***
+ * Function methods
  */
 
-let x = {'a': 1, 'b': 2};
-let y = x;
-// console.log(x);
-// console.log(y);
-x.a = 3;
-// console.log(x);
-// console.log(y);
+// let xx = {'a': 5, 
+//     'b': 'hello',
+//     'add': function(x, y) {
+//         return x + y + this.a;
+//     }
+// };
 
-let z = {...x};
-x.a = 5;
-console.log(x);
-console.log(y);
-console.log(z);
+// console.log(xx.add(3,4));
+// let z = xx.add;
+// call()
+// console.log(z.call("", 3, 4));
+// console.log(z.call(xx, 3, 4));
+
+// apply() - spreads the arguments - extra ignored
+
+// console.log(z.apply(xx, [1, 2, 3, 4]));
+
+// bind() - closure
+
+// let z2 = z.bind(xx, 2);
+// console.log(z2(3));
+
+/***
+ * Prototypes
+ */
+
+// const x = {a: 1, inc: function() {this.a++;}};
+// console.log(x);
+// const y = {__proto__: x, b: 2};
+// console.log(y);
+// console.log(y.a);
+// y.inc();
+// console.log(y.a);
+
+/***
+ * Classes
+ */
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    describe() {
+        return `${this.name} makes a sound ${this.sound}`;
+    }
+}
+
+let x = new Animal('Jerry');
+console.log(x.describe());  // Jerry makes a sound undefined
+
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+        this.sound = 'woof';
+    }
+}
+
+let y = new Dog('Momo');
+console.log(y.describe());  // Momo makes a sound woof
+
+class Cat extends Animal {
+    constructor(name) {
+        super(name);
+        this.sound = 'meow';
+    }
+    static fromJson(o) {
+        c = new Cat(o.name);
+        c.sound = o.sound;
+        return c;
+    }
+}
+
+let c = new Cat('Miffy');
+console.log(c.describe());  // Miffy makes a sound meow
