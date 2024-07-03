af05176b772d5aa024660f6c601ff41c2ffddf6e
ara <22f3000701@ds.study.iitm.ac.in>
2024-07-03 05:51:53 +0530

    moved notes to notes repo - only code here

--- week 2/collections.js
+++ week 2/collections.js
@@ -1,96 +1,86 @@
+/***
+ * Basic array declaration - mixed mode 
+ */
 // let x = [1, 2, 3];
+// console.log(`${typeof(x)}: ${x} with length = ${x.length}`); // object: 1,2,3 with length = 3
+// console.log(x[0]); //  1
 
-// `` is a template string
-// within the template string, any code inside ${} will get evaluated
-
-// console.log(`${typeof(x)}: ${x} with length = ${x.length}`);
-
-// here ${typeof(x)} should run the typeof function on x, and print out the type of x (which is an array) - but to JS, an array is an object
-// ${x} should print out the array itself
-// ${x.length} should print out the length of the array
-
-// console.log(x[0]);
-// should pick out the first element of the array - 0 indexed like Python
-
-// Mixed element types
-
+// // Mixed element types
 // let y = [1, 'b', a => a + 1];
-// console.log(`${typeof(y)}: ${y} with length = ${y.length}`);
+// console.log(`${typeof(y)}: ${y} with length = ${y.length}`); // object: 1,b,a => a + 1 with length = 3
+// console.log(x.length, y.length); // 3 3
 
-// y[2] is a function - takes an argument a and returns a + 1
-// a is of type any, so it can be any type, JS is not statically typed
-
-// console.log(x.length, y.length);
-
-// Deleting - Null out an entire collection
-
+// // Deleting
 // x = [];
-// console.log(`${typeof(x)}: ${x} with length = ${x.length}`);
+// console.log(`${typeof(x)}: ${x} with length = ${x.length}`); // object:  with length = 0
 
-// x continues to be an object - but it is now an empty object with length 0
-
-// Holes
-
+// // Holes
 // y.length = 10;
-// console.log(`${typeof(y)}: ${y} with length = ${y.length}`);
+// console.log(`${typeof(y)}: ${y} with length = ${y.length}`); // object: 1,b,a => a + 1,,,,,,, with length = 10
 
-// normally, in Python for example, the length can't be directly set like this
-// here, the length is set to 10, but the first 3 elements are from above and the rest are undefined or null - depends on the implementation
-
-// L2.2
 
 /*** 
  * Iteration
 */
-
 // let x = [1, 'b', a => a + 1];
-
 // x.length = 5;
-
 // for (let i=0; i<x.length; i++) {
 //     console.log(`x: ${x[i]} of type ${typeof(x[i])}`);   
 // }
+// x: 1 of type number
+// x: b of type string
+// x: a => a + 1 of type function
+// x: undefined of type undefined
+// x: undefined of type undefined
 
-
+// 'in' iteration - skips undefined entries
 // for (const i in x) {
 //     console.log(`x: ${x[i]} of type ${typeof(x[i])}`);   
 // }
+// x: 1 of type number
+// x: b of type string
+// x: a => a + 1 of type function
 
-// here i is the index of the array being iterated over so only get the not null entries in x using const
-
-
+// 'of' iteration - takes values
 // for (const i of x) {
 //     console.log(`x: ${i} of type ${typeof(i)}`);   
 // }
+// x: 1 of type number
+// x: b of type string
+// x: a => a + 1 of type function
+// x: undefined of type undefined
+// x: undefined of type undefined
 
 /***
  * Are objects iterable?
  */
-
 // let x = {'a': 1, 'b': 'alpha', 'c': [3, 2, 1]};
-
-// iterates over keys in x
 // for (const i in x) {
 //     console.log(i);
-//     console.log(x[i]);
+//     // console.log(x[i]);
 // }
 
 // tries to iterate directly over values of x - not iterable for const i of x
 // for (const i of x) {
-//     console.log(i);
-//     console.log(x[i]);
+//     console.log(i); // TypeError: x is not iterable
+//     // console.log(x[i]);
 // }
 
 // for (const [k, v] of Object.entries(x)) {
-//     console.log(v);
+//     // console.log(v);
+//     console.log(k, v);
 // }
+
+/***
+ * Creating arrays with holes
+ */
 
 // let x = new Array(5);
 // x[1] = 10;
 // x[3] = 'hello';
-// // for (const [k, v] of x.entries()) {
-// //     console.log(`Index ${k}, value ${v} of type ${typeof(v)}`);
-// // }
+// for (const [k, v] of x.entries()) {
+//     console.log(`Index ${k}, value ${v} of type ${typeof(v)}`);
+// }
 
 // in operator only iterates over defined values
 // for (const i in x) {
@@ -112,10 +102,10 @@
  *  Iteration and Transformation
  */
 
-// let x = [1, -2, 3, -4, 5, 6, -7, 8];
-// let y = x.find (t => t < 0);
-// console.log(x);
-// console.log(y);
+let x = [1, -2, 3, -4, 5, 6, -7, 8];
+let y = x.find (t => t < 0);
+console.log(x);
+console.log(y);
 
 // find returns the first element that satisfies the condition - here, the first negative number in x. so y = -2
 
@@ -123,11 +113,11 @@
 // x inside the filter function is a different x from the one outside - different scope
 // better to use different variable names to avoid confusion
 
-// console.log(x.filter(t => t < 0));
+console.log(x.filter(t => t < 0));
 
 // filter returns all elements that satisfy the condition - here, all negative numbers in x. so y = [-2, -4, -7]
 
-// console.log(x.map(i => i > 0 ? '+' : '-'));
+console.log(x.map(i => i > 0 ? '+' : '-'));
 
 // map applies the function to all elements of the array - here, if the element is positive, it returns '+', otherwise it returns '-'
 // so output is ['+', '-', '+', '-', '+', '+', '-', '+']
